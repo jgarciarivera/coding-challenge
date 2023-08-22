2ef11c0a7db8a0481f22ef6a3d23a4f8d5769c53
Jorge Garcia-Rivera <garciariverajorge@gmail.com>
2023-08-22 13:45:49 -0500

    WIP add person functionality

--- umbrage/src/components/People.jsx
+++ umbrage/src/components/People.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Card, Button, Modal, Row, Col } from "react-bootstrap";
+import { Card, Button, Modal, Row, Form } from "react-bootstrap";
 
 export const People = ({ token }) => {
   const [people, setPeople] = useState([]);
   const [person, setPerson] = useState({
     person: { comments: [{ comment: "" }] },
   });
-  const [showModal, setShowModal] = useState(false);
+
+  const [showSelectedPersonModal, setShowSelectedPersonModal] = useState(false);
   const [selectedPersonId, setSelectedPersonId] = useState("");
+
+  const [showCreateNewPersonModal, setShowCreateNewPersonModal] =
+    useState(false);
+  const [newPersonFirstName, setNewPersonFirstName] = useState("");
+  const [newPersonLastName, setNewPersonLastName] = useState("");
+  const [newPersonEmail, setNewPersonEmail] = useState("");
+  const [newPersonJobTitle, setNewPersonJobTitle] = useState("");
+  const [newPersonAvatar, setNewPersonAvatar] = useState("");
 
   useEffect(() => {
     const fetchPeople = async () => {
@@ -73,9 +82,51 @@ export const People = ({ token }) => {
     fetchPerson();
   }, [selectedPersonId]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("https://umbrage-interview-api.herokuapp.com/people", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        first_name: newPersonFirstName,
+        last_name: newPersonLastName,
+        job_title: newPersonJobTitle,
+        email: newPersonEmail,
+        avatar: newPersonAvatar,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Failed to add Person. Request returned the following response: "${response.status} ${response.statusText}"`
+          );
+        }
+        // Refresh list of people
+        alert("Successfully added new Person!");
+        setNewPersonFirstName("");
+        setNewPersonLastName("");
+        setNewPersonJobTitle("");
+        setNewPersonEmail("");
+        setNewPersonAvatar("");
+        setShowCreateNewPersonModal(false);
+        return response.json();
+      })
+      .catch((error) => {
+        console.error("Error", error);
+        alert(error.message);
+      });
+  };
+
   return (
     <div>
-      <Modal centered show={showModal} onHide={() => setShowModal(false)}>
+      <Modal
+        centered
+        show={showSelectedPersonModal}
+        onHide={() => setShowSelectedPersonModal(false)}
+      >
         <Modal.Header closeButton>
           <Modal.Title>
             Comments for {person.first_name} {person.last_name}
@@ -85,6 +136,72 @@ export const People = ({ token }) => {
           {person.comments?.map((comment, index) => (
             <li key={index}>{comment.comment}</li>
           ))}
+        </Modal.Body>
+      </Modal>
+      <Modal
+        centered
+        show={showCreateNewPersonModal}
+        onHide={() => setShowCreateNewPersonModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Person</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <input
+              className="new-person-modal-input"
+              type="text"
+              value={newPersonFirstName}
+              onChange={(e) => setNewPersonFirstName(e.target.value)}
+              placeholder="First Name"
+              autoComplete="off"
+            ></input>
+            <input
+              className="new-person-modal-input"
+              type="text"
+              value={newPersonLastName}
+              onChange={(e) => setNewPersonLastName(e.target.value)}
+              placeholder="Last Name"
+              autoComplete="off"
+            ></input>
+            <input
+              className="new-person-modal-input"
+              type="text"
+              value={newPersonJobTitle}
+              onChange={(e) => setNewPersonJobTitle(e.target.value)}
+              placeholder="Job Title"
+              autoComplete="off"
+            ></input>
+            <input
+              className="new-person-modal-input"
+              type="text"
+              value={newPersonEmail}
+              onChange={(e) => setNewPersonEmail(e.target.value)}
+              placeholder="Email"
+              autoComplete="off"
+            ></input>
+            <input
+              className="new-person-modal-input"
+              type="text"
+              value={newPersonAvatar}
+              onChange={(e) => setNewPersonAvatar(e.target.value)}
+              placeholder="Avatar URL"
+              autoComplete="off"
+            ></input>
+            <button
+              disabled={
+                !newPersonFirstName ||
+                !newPersonLastName ||
+                !newPersonJobTitle ||
+                !newPersonEmail
+              }
+              className="login-button"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Confirm
+            </button>
+          </Form.Group>
         </Modal.Body>
       </Modal>
       <div className="people-header">People</div>
@@ -115,7 +232,7 @@ export const People = ({ token }) => {
                 }}
                 onClick={() => {
                   setSelectedPersonId(person.id);
-                  setShowModal(true);
+                  setShowSelectedPersonModal(true);
                 }}
               >
                 Details
@@ -124,6 +241,14 @@ export const People = ({ token }) => {
           </Card>
         ))}
       </Row>
+      <button
+        className="add-person-button"
+        onClick={() => {
+          setShowCreateNewPersonModal(true);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
